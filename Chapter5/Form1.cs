ae2242052239a3af51f159f3fd2113f11c01193b
omnis <40761437+ioklkiol@users.noreply.github.com>
2020-05-01 12:05:03 +0800

    添加第五章

--- Chapter5/Form1.cs
+++ Chapter5/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Chapter4
+namespace Chapter5
 {
     public partial class Form1 : Form
     {
@@ -16,6 +16,15 @@ namespace Chapter4
         {
             InitializeComponent();
         }
+        //得到一个0到1的随机数
+        private double RandomDouble()
+        {
+            var seed = Guid.NewGuid().GetHashCode();    //使用Guid类得到一个接近唯一的随机数种子
+            Random r = new Random(seed);
+            int i = r.Next(0, 100000);
+            return (double)i / 100000;
+        }
+
         private Vector3D GetColor(Ray r, HittableList world)
         {
             HitRecord rec;
@@ -30,10 +39,13 @@ namespace Chapter4
                 return (1 - t) * new Vector3D(1, 1, 1) + t * new Vector3D(0.5, 0.7, 1);
             }
         }
+    
         private void Form1_Load(object sender, EventArgs e)
         {
             int nx = 200;
             int ny = 100;
+            int ns = 100;                         //设置采样率抗锯齿，但渲染所需要的时间也变长了，获得随机数使用了大量时间,
+                                                  //有时间的话可以优化一下函数RandomDouble
             Bitmap bmp = new Bitmap(nx, ny);
             Vector3D lowerLeft = new Vector3D(-2, 1, -1);
             Vector3D horizontal = new Vector3D(4, 0, 0);
@@ -44,20 +56,25 @@ namespace Chapter4
             list.Add(new Sphere(new Vector3D(0, 0, -1), 0.5));
             list.Add(new Sphere(new Vector3D(0, -100.5, -1), 100));
             HittableList world = new HittableList(list, 2);
+            Camera cam = new Camera();
             for (int j = 0; j < ny; j++)
             {
                 for (int i = 0; i < nx; i++)
                 {
-                    double u = (double)i / (double)nx;
-                    double v = (double)j / (double)ny;
-                    Ray ray = new Ray(origin, lowerLeft + u * horizontal + v * vertical);
-                    Vector3D p = ray.GetPoint(2);
-                    Vector3D color = GetColor(ray, world);    
+                    Vector3D color = new Vector3D(0, 0, 0);
+                    for (int s = 0; s<ns; s++)
+                    {
+                        double u = (double)(i+RandomDouble()) / (double)nx;
+                        double v = (double)(j+RandomDouble()) / (double)ny;
+                        Ray ray = cam.GetRay(u,v);
+                        color += GetColor(ray, world);      //将所有采样点的颜色相加
+                    }
+                    color /= ns;                            //除以采样点的数量得到平均值
                     int r = (int)(255 * color.X);
                     int g = (int)(255 * color.Y);
                     int b = (int)(255 * color.Z);
-                    bmp.SetPixel(i, j, Color.FromArgb(r, g, b));   //如果之前没有将判别式除以4的话这里会报错，则需要限定一下边界，
-                                                                   //将rgb的值限制在0到255之间，会得到一个和书上不一样的球
+                    bmp.SetPixel(i, j, Color.FromArgb(r, g, b));
+
                 }
 
             }
