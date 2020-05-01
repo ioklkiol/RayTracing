a9c6ed7509aa882fb0995a6a975265f54e4892bc
omnis <40761437+ioklkiol@users.noreply.github.com>
2020-05-01 22:41:56 +0800

    添加第十二章

---
+++ Chapter12/Hittable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public abstract class Hittable
+{
+   public abstract bool Hit(Ray r, double tMin, double tMax,out HitRecord rec);
+} 
+
